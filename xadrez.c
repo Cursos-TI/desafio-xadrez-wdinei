d9ef4855d8d587b62ae3186253f103a5eb4afbfe
wdinei <wdinei@gmail.com>
2026-08-15 23:09:32 +0000

    Movimentação de peças de xadrez - Desafio: nível novato

--- xadrez.c
+++ xadrez.c
@@ -4,6 +4,35 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
+const int numeroDeMovimentos = 5;
+
+char* getDirecao(char direcao){
+    switch (direcao){
+        case 'c': return "Cima";
+        case 'b': return "Baixo";
+        case 'e': return "Esquerda";
+        case 'd': return "Direita";
+        default: return "Direção inválida!";
+    }
+}
+
+/* direcao pode ser C,B,E ou D */
+void moveHorizontalVertical(char direcao){
+    int i;
+    for (i = 1; i <= numeroDeMovimentos; i++){
+        printf("%s\n", getDirecao(direcao));
+    }
+}
+
+/* direcao pode ser C,B,E ou D */
+void moveDiagonal(char direcaoH, char direcaoV){
+    int i = 1;
+    while (i <= numeroDeMovimentos){
+        printf("%s/%s\n", getDirecao(direcaoH), getDirecao(direcaoV));
+        i++;
+    }
+}
+
 int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
@@ -27,6 +56,9 @@ int main() {
 
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
     // Inclua o uso de continue e break dentro dos loops.
+
+    moveHorizontalVertical('c');
+    moveDiagonal('e', 'c');
 
     return 0;
 }
